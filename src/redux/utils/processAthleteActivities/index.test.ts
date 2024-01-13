120242a9269f3c625952ff2b45057d75552b6019
Lewis Watt <L.Watt-21@student.lboro.ac.uk>
2024-01-13 18:19:43 +0000

    add more to athlete activities state to fix dashboard

--- src/redux/utils/processAthleteActivities/index.test.ts
+++ src/redux/utils/processAthleteActivities/index.test.ts
@@ -8,12 +8,16 @@ describe("processAthleteActitivies", () => {
 				distance: "5000",
 				map: { summary_polyline: "_p~iF~ps|U_ulLnnqC_mqNvxq`@" },
 				id: 1,
+				athlete: { id: 1 },
+				start_date: "2020-01-01T00:00:00Z",
 			},
 			{
 				name: "Bike ride",
 				distance: "15000",
 				map: { summary_polyline: "_ibE_seK_seK_seK" },
 				id: 2,
+				athlete: { id: 2 },
+				start_date: "2020-01-01T00:00:00Z",
 			},
 		]
 		const expected = [
@@ -22,12 +26,16 @@ describe("processAthleteActitivies", () => {
 				distance: "5000",
 				polyline: "_p~iF~ps|U_ulLnnqC_mqNvxq`@",
 				id: 1,
+				athlete: { id: 1 },
+				startDate: "2020-01-01T00:00:00Z",
 			},
 			{
 				name: "Bike ride",
 				distance: "15000",
 				polyline: "_ibE_seK_seK_seK",
 				id: 2,
+				athlete: { id: 2 },
+				startDate: "2020-01-01T00:00:00Z",
 			},
 		]
 
--- src/redux/utils/processAthleteActivities/index.ts
+++ src/redux/utils/processAthleteActivities/index.ts
@@ -2,15 +2,21 @@ export interface Activity {
 	name: string
 	distance: string
 	map: { summary_polyline: string }
+	athlete: { id: number }
 	id: number
+	start_date: string
 }
 
 export const processAthleteActivities = (data: Activity[]) => {
 	const processedData = data.map((activity: Activity) => ({
+		athlete: {
+			id: activity.athlete.id,
+		},
 		name: activity.name,
 		distance: activity.distance,
 		polyline: activity.map.summary_polyline,
 		id: activity.id,
+		start_date: activity.start_date,
 	}))
 	return processedData
 }
